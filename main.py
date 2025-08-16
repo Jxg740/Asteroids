51a96b418d50a7ee22f49cd52636930cbbc9134d
Joe <jxgierk740@gmail.com>
2025-08-16 12:00:40 -0600

    Create drawable and updatable object groups

--- main.py
+++ main.py
@@ -14,19 +14,23 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = (SCREEN_WIDTH / 2), y = (SCREEN_HEIGHT / 2) )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        
+
+    while True:        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        
+        updatable.update(dt)
+
 if __name__ == "__main__":
     main()
